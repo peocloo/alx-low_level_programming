4cb62011248928b95fb52ca795b2c2a438f4b4a4
peocloo <philemoneyramocloo@gmail.com>
2023-10-05 19:47:51 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
-/*main function 
- * returns 0*/
+/*
+ * main function 
+ * returns 0
+ * */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
