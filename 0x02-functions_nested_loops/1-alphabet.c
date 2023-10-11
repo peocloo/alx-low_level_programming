b40183ed7dc6b8c5245c780c46261512d104cb50
peocloo <philemoneyramocloo@gmail.com>
2023-10-11 03:35:15 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point 
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	/*declare variable and initialize it with first member of the range*/
+	char alphabet = 'a';
+
+	/*check to see if value is the same as last character*/
+	while(alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
+	return;
+}
