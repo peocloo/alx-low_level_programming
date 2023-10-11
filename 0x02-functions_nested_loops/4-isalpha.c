a870300d4e3b69d35d3546ff295a2e26737c8a77
peocloo <philemoneyramocloo@gmail.com>
2023-10-11 09:41:13 -0700

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalph - function that checks for an alphabet/
+ * @c: integer argument for function
+ * Return: integer 0.
+ */
+
+int _isalpha(int c)
+{
+	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A')) 
+		return (1);
+	else
+		return (0);
+}
