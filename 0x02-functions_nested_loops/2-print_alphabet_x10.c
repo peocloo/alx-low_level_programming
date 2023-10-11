57d922dee232b57c755a6689f310c115f013e563
peocloo <philemoneyramocloo@gmail.com>
2023-10-11 04:34:58 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_alphabet_x10- prints alphabets in lowercase 10x
+ * Return: void;
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	while (i<10)
+	{
+		/*declare variable and assign begining of alphabet*/
+		char alphabet = 'a';
+
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
