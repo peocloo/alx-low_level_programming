077fba5e67c3cccb7a10a4e0619dc9ef1a7217d9
peocloo <philemoneyramocloo@gmail.com>
2023-10-11 10:33:52 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *@n:int argument for function
+ *Return: integer
+ *print_sign- function that prints sign of number.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+	
