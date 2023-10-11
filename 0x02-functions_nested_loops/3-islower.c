e91031abcbfff66262f1383f90b573629168f388
peocloo <philemoneyramocloo@gmail.com>
2023-10-11 05:50:03 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,13 @@
 /**
  * _islower()- checks for lower character.
  * Return: integer 0 or 1.
+ * c = argument for function.
  */
 
 int _islower(int c)
 {
 	/*check if ch is lower or uppercase*/
-	if (c <= 'Z' || c >= 'A')
+	if (c <= 'z' && c >= 'a')
 		return (1);
 	else
 		return (0);
