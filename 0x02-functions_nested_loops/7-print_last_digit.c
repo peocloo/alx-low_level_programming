b7f8f08a1d85152ab2f4e3ed67db303b7b17da03
peocloo <philemoneyramocloo@gmail.com>
2023-10-11 11:51:08 -0700

    7-print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function to print las digits
+ * @n - argument of type int.
+ * return: integer of n
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+	return (last_digit);
+}
