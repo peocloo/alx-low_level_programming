eeec066691054050206463be148a31c35428a192
peocloo <philemoneyramocloo@gmail.com>
2023-10-13 17:54:05 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -8,4 +9,3 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 #endif
-
