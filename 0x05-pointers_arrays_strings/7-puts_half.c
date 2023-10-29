7472625657d382a2a69c6b5343d64c54acf63447
peocloo <philemoneyramocloo@gmail.com>
2023-10-29 05:08:56 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,13 +8,20 @@
  */
 void puts_half(char *str)
 {
-	int i, length, n;
+	int a, n, l;
 
-	for (i = 0; str[i] != '\0'; i++)
-		length = i;
+	l = 0;
 
-	n = (length - 1) / 2;
-	for (i = (n + 1); str[i] != '\0'; i++)
-		_putchar(str[i]);
+	for (a = 0; str[a] != '\0'; a++)
+		l++;
+
+	n = (l / 2);
+
+	if ((l % 2) == 1)
+		n = ((l + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
 	_putchar('\n');
 }
+
