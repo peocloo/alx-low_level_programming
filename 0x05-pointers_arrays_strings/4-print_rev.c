e9d0102a755c9d657c599614c99245ffa7d5217b
peocloo <philemoneyramocloo@gmail.com>
2023-10-29 01:49:10 -0700

    adding 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,6 +9,7 @@
 void print_rev(char *s)
 {
 	int i, j;
+
 	int len = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
-int _putchar(char c);
 /**
- * rev_string - prints a string in reverse
- * @s: pointer to a char argument
+ * void rev_string(char *s);
+ * @s: pointer to char argument
  *
  * Return: void
  */
 void rev_string(char *s)
 {
-	int i, j, len = 0;
+	int i, j; 
+	int len;
+	char str[] = "";
+
+	len = 0;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		len = i;
-	}
-	for (j = len; j > 0; j--)
+	}	
+
+	for (j = len; j >= 0; j--)
 	{
-		_putchar(s[j]);
+		s[] += s[j];
 	}
-	_putchar('\n');
 }
