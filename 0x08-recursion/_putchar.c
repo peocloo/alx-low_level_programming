2d0272d1ce493995763253541c6251c237301013
peocloo <philemoneyramocloo@gmail.com>
2023-10-29 19:10:09 -0700

    T1

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes character to stdout
+ * @c: character argument
+ *
+ * Return: On success 1
+ * on error -1, and errno is set appropriately
+ */
+int void(main)
+{
+	return (write(1, &c, 1));
+}
