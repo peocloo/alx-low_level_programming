d9a52f742fb4b2f815ecd93a6824b4ae447fcaa0
peocloo <philemoneyramocloo@gmail.com>
2023-11-01 14:08:32 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - print number of arguments passed into program
+ * @argc: integer argument
+ * @argv: pointer to char argument
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
