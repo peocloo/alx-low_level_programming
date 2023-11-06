72d8916c9d35240db3286bca49bc3a810ad18291
peocloo <philemoneyramocloo@gmail.com>
2023-11-06 08:45:04 -0800

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,8 +2,8 @@
 #include "dog.h"
 
 /**
- * int_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
  * @name: name to initialize
  * @age: age to initialize
  * @owner: owner to initialize
