7297364dfc6c1c5a4b96341931908f0a4f0c8934
peocloo <philemoneyramocloo@gmail.com>
2023-11-09 15:00:59 -0800

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,25 +9,26 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	va_list args;
+        unsigned int i;
+        va_list args;
 
-	va_start(args, n);
+        va_start(args, n);
 
-	for (i = 0; i < n; i++)
-	{
-		int x = va_arg(args, const unsigned int);
+        for (i = 0; i < n; i++)
+        {
+                int x = va_arg(args, const unsigned int);
 
-		if (separator == NULL)
-		{
-			printf("%d", x);
-		}
-		else
-		{
-			printf("%d", x);
-			printf("%s", separator);
-		}
-	}
-	printf("\n");
-	va_end(args);
+                if (separator == NULL)
+                {
+                        printf("%d", x);
+                }
+                else
+                {
+                        printf("%d", x);
+                        printf("%s", separator);
+                }
+        }
+        printf("\n");
+
+        va_end(args);
 }
